7a760e74eef19b680a91f2a13bfc56d70015cbc9
Stojj2 <stojj2@hotmail.com>
2024-09-15 19:38:46 +0000

    Addng a first test view to views.py for verifying Django functionality

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+# Test function for Django setup
+def say_hello(request):
+    return HttpResponse("Hello! Django seems to work just fine.")
