9d45c19dc3d57a31b4e6b26decd8ad82b7c861fb
Stojj2 <stojj2@hotmail.com>
2024-09-23 16:23:37 +0000

    Change static files location in settings.py

--- Project_Website/settings.py
+++ Project_Website/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Gallery', 'static'),
+    BASE_DIR / 'Gallery' / 'static',  # This points to your static directory
 ]
 
 # Default primary key field type
